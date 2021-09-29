3a6d52295cb73dc0a70bc17505cab024a42c25a9
anurag <soni.anurag300@gmail.com>
2021-09-29 15:33:09 +0530

    login done

--- lib/screens/register/register_error.dart
+++ lib/screens/register/register_error.dart
@@ -1,6 +1,7 @@
 import 'package:ddd/backend/cubit/loginuser_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RegisterError extends StatelessWidget {
   const RegisterError({Key? key}) : super(key: key);
@@ -13,7 +14,19 @@ class RegisterError extends StatelessWidget {
           return Container();
         }
         if (state is LoginuserExist) {
-          return Center(child: Text('User exist'));
+          return Center(
+            child: Text(
+              'User exist',
+              style: GoogleFonts.shadowsIntoLight(
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  height: 1.5,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          );
         } else
           return Container();
       },
